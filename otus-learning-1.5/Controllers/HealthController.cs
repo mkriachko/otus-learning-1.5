cfc6500b73baac271e22cf26f8002f7d80242311
Mikhail Kriachko <mvkriachko@gmail.com>
2021-07-19 10:55:48 +0300

    add simple service

---
+++ otus-learning-1.5/Controllers/HealthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace otus_learning_1._5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { status = "OK" });
+        }
+
+        [HttpGet("ready")]
+        public IActionResult GetReadyness()
+        {
+            return Ok(new { ready = true });
+        }
+    }
+}
